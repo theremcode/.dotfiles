b7a99ac51ec9212b416f366233ab440636d88e0e
theremcode <105158018+theremcode@users.noreply.github.com>
2024-05-01 22:02:06 +0200

    update sh

--- config-ubuntu.sh
+++ config-ubuntu.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+
 # Array of tools to install
 tools=("wget" "svn" "zsh")
+
+# Update package lists once
+sudo apt update -y
 
 # Loop through the array and check if each tool is installed
 for tool in "${tools[@]}"; do
     if ! [ -x "$(command -v $tool)" ]; then
         # Install the tool
-        sudo apt update -y
-        sudo apt update install $tool -y
+        sudo apt install $tool -y
     fi
 done
 
-# check if starship is installed and if not install it
+# Check and install Starship
 if ! [ -x "$(command -v starship)" ]; then
     curl -sS https://starship.rs/install.sh | sh
     echo 'eval "$(starship init bash)"' >> ~/.bashrc
@@ -19,19 +22,14 @@ if ! [ -x "$(command -v starship)" ]; then
     wget -O ~/.config/starship.toml https://raw.githubusercontent.com/theremcode/.dotfiles/main/.config/starship.toml
     source ~/.bashrc
 fi
-    
-# check if oh my zsh is installed and if not install it
+
+# Install Oh My Zsh if not installed
 if ! [ -d ~/.oh-my-zsh ]; then
     sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
     wget -O ~/.zshrc https://raw.githubusercontent.com/theremcode/.dotfiles/main/.zshrc
 fi
 
-# Check if the line exists in .bashrc
-if grep -q "source ~/.zshrc" ~/.bashrc; then
-    echo "The line 'source ~/.zshrc' is already in your .bashrc file."
-else
-    # If the line doesn't exist, add it to .bashrc
-    echo "Adding 'source ~/.zshrc' to your .bashrc file..."
+# Ensure .zshrc is sourced from .bashrc
+if ! grep -q "source ~/.zshrc" ~/.bashrc; then
     echo "source ~/.zshrc" >> ~/.bashrc
-    echo "Line added successfully."
 fi
