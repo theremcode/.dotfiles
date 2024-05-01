98bacb00541d46c95793f35e113a2e53c338ccbb
theremcode <105158018+theremcode@users.noreply.github.com>
2024-05-01 20:19:04 +0200

    update

--- config-ubuntu.sh
+++ config-ubuntu.sh
@@ -1,5 +1,13 @@
+#!/bin/bash
+
+# Install Starship
 curl -sS https://starship.rs/install.sh | sh
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 source ~/.bashrc
+
+# Install Oh My zsh
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+
 mkdir -p ~/.config
 wget -O ~/.config/starship.toml https://raw.githubusercontent.com/alexislepresle/dotfiles/main/starship.toml
