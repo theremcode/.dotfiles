bd491574a67fb0b4c093690dcc53830f7c8ab48f
theremcode <105158018+theremcode@users.noreply.github.com>
2024-05-01 21:12:21 +0200

    bashrc

--- config-ubuntu.sh
+++ config-ubuntu.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # check if wget is install and if not install it
 if ! [ -x "$(command -v wget)" ]; then
-    sudo apt update
-    sudo apt install wget
+    sudo apt update -y
+    sudo apt install wget -y
 fi
 
 # check if starship is installed and if not install it
@@ -16,13 +16,21 @@ fi
 
 # check if zsh is installed and if not install it
 if ! [ -x "$(command -v zsh)" ]; then
-    sudo apt install zsh
+    sudo apt install zsh -y
 fi
     
 # check if oh my zsh is installed and if not install it
 if ! [ -d ~/.oh-my-zsh ]; then
     sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
     wget -O ~/.zshrc https://raw.githubusercontent.com/theremcode/.dotfiles/main/.zshrc
-    source ~/.zshrc
 fi
 
+# Check if the line exists in .bashrc
+if grep -q "source ~/.zshrc" ~/.bashrc; then
+    echo "The line 'source ~/.zshrc' is already in your .bashrc file."
+else
+    # If the line doesn't exist, add it to .bashrc
+    echo "Adding 'source ~/.zshrc' to your .bashrc file..."
+    echo "source ~/.zshrc" >> ~/.bashrc
+    echo "Line added successfully."
+fi
