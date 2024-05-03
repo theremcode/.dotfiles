bba843004d8d475a8e97f79a85e6260e5dd1cdd7
theremcode <105158018+theremcode@users.noreply.github.com>
2024-05-03 15:25:14 +0200

    aliases

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -8,13 +8,3 @@ alias a="ansible"
 alias ap="ansible-playbook"
 
 alias code="open -a 'Visual Studio Code'"
-
-alias gprj="cd $HOME/Projects"
-alias gcs="cd $HOME/Projects/christianlempa/cheat-sheets"
-alias gbp="cd $HOME/Projects/christianlempa/boilerplates"
-
-alias ls="eza --icons --group-directories-first"
-alias ll="eza --icons --group-directories-first -l"
-#alias grep='grep --color'
-
-alias prx="ssh root@prx-prod-2.home.clcreative.de"
